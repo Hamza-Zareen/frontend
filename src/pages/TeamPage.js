ca15ee44c71194dcf9db8ecaac825e348f72a846
hamza-zareen <hamza.zareen007@gmail.com>
2021-05-23 01:16:25 +0500

    added some more styling

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -2,7 +2,7 @@ import { React, useEffect, useState } from 'react';
 import { MatchDetailsCard } from '../components/MatchDetailsCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 import { useParams } from 'react-router-dom';
-
+import { PieChart } from 'react-minimal-pie-chart';
 import './TeamPage.scss';
 
 export const TeamPage = () => {
@@ -33,14 +33,22 @@ if(!team || !team.teamName){
     <div className="team-name-section" >
       <h1 className="team-name">{team.teamName}</h1>
       </div>
-     <div className="win-loss-section">Wins / Loses</div> 
+     <div className="win-loss-section">
+       Wins / Loses
+       <PieChart
+  data={[
+    { title: 'Loss', value: team.totalMatches-team.totalWins, color: '#a34d5d' },
+    { title: 'Win', value: team.totalWins, color: '#4da375' },
+  ]}
+/>
+       </div> 
      <div className="match-detail-section">
         <h3>Latest Matches</h3>
         <MatchDetailsCard teamName= {team.teamName} match={team.matches[0]}/>
        </div> 
       {team.matches.slice(1).map(match => <MatchSmallCard teamName= {team.teamName} match={match}/>)}
-    <div>
-      <a href="#">More</a>
+    <div className="more-link">
+      <a href="#">More ></a>
     </div>
     </div>
   );
